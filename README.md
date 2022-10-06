3cabf5b87308e0a6d7d349cbb5471b7513148f26
Kseniya <sh.ksusha_0303@mail.ru>
2022-10-06 15:18:58 +0300

    fix: reame script

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ToDoList
+<h1 align="center">Hi there, I'm <a href="https://github.com/ZGabanz" target="_blank">Kseniya</a> 
+<img src="https://github.com/blackcater/blackcater/raw/main/images/Hi.gif" height="32"/></h1>
+<h2 align="center"><a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&pause=1000&color=F74C8B&center=true&width=435&lines=Computer+science+student" alt="Typing SVG" /></a></h2>
+
+[![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=ZGabanz&show_icons=true&theme=radical)](https://github.com/anuraghazra/github-readme-stats)
+
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ZGabanz)](https://github.com/anuraghazra/github-readme-stats)
+
+
--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 const input = document.querySelector('#input');
 const btnClear = document.querySelector('#btn');
 const result = document.querySelector('#result');
+const inp = document.getElementById("input");
 
-var inp = document.getElementById("input");
-    inp.addEventListener("keyup", function(event) {
-        event.preventDefault();
-        if (event.keyCode === 13) {
-            createElements(input.value)
-            input.value = ''
-        }
-    });
+inp.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        createLists(input.value)
+        input.value = ''
+    }
+});
 
-function createElements(value) {
-    console.log(value);
+function createLists(text) {
 
     const li = document.createElement('li');
     li.setAttribute("type","checkbox");
     li.className = 'li';
-    li.textContent = value;
+    li.textContent = text;
 
     btnClear.addEventListener('click', (e) => {
         document.body.removeChild(li);
